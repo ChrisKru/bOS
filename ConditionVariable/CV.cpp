0a580e52bbd6c049d129da4cc425a275f5c6d216
Sławke <slawomir.lyskawinski@gmail.com>
2017-12-23 18:45:42 +0100

    CV - wersja (prawdopodobnie) finalna

--- ConditionVariable/CV.cpp
+++ ConditionVariable/CV.cpp
@@ -12,19 +12,23 @@ bool CV::wait() {
 		if (pcb_waiting_list.size() > 0) {
 			if (pcb_waiting_list.front()->GetID() != running->GetID()) {
 				running->SetState(State::OCZEKUJACY);
+				pcb_waiting_list.push_back(running);
 			}
 		}
 	}
-	pcb_waiting_list.push_back(running);
-
+	else {
+		pcb_waiting_list.push_back(running);
+	}
 	return temp;
 }
 void CV::signal() {
 	pcb_waiting_list.pop_front();
 	pcb_waiting_list.remove_if([](std::shared_ptr<PCB> proces) {return proces->GetState() == State::ZAKONCZONY; });
 
-	if (pcb_waiting_list.size() > 0)
+	if (pcb_waiting_list.size() > 0) {
 		pcb_waiting_list.front()->PCB::SetState(State::GOTOWY);
+		dodaj_do_procesow_gotowych(pcb_waiting_list.front());
+	}
 }
 bool CV::is_empty() {
 	if (pcb_waiting_list.size() == 0)
